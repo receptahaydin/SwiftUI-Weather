658ba3bb1cc12cf5bcfd906b27e44355d5f73eba
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-05-14 01:05:53 +0300

    Application completed

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Recep Taha Aydın on 30.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
